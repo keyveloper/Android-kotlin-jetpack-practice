8f9969c19927db87ccdbd5da7d09e5c7de64c657
zen <didehdrb21@naver.com>
2024-09-14 14:11:20 +0900

    topHeader maded : practice parameter & structure

--- app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
@@ -4,64 +4,83 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpack_compose_practice.ui.theme.JetpackcomposepracticeTheme
+import com.example.jetpack_compose_practice.ui.theme.DefaultTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { // enter point for emulator // not preview
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetpackcomposepracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            DefaultTheme {
+                MyApp {
+                    Text("Hello Again!!")
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DefaultPreview() {
+    DefaultTheme {
+        MyApp {
+            Text("Hello Agai1n")
+        }
+    }
 }
 
 @Composable
-fun ShowAge(age: Int = 12) {
-    Text(text = age.toString())
+fun MyApp(content: @Composable () -> Unit) {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        content()
+    }
 }
 
-@Preview(name = "test name ",showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    JetpackcomposepracticeTheme(darkTheme = true) { // there is no layout
-        Surface(
-            modifier = Modifier.fillMaxWidth()
-                .fillMaxHeight()
-                .padding(all = 59.dp),
-            color = MaterialTheme.colorScheme.primary) { // total surface define
-            Column() {
-            Greeting("Android")
-            ShowAge(13)
-                //
-        } }
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
+        ,
+        // .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))
+        color = Color(color = 0xFFE9D7f7)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp)
+            ,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson) // for format
+            Text(text = "Total Per Person",
+                style = MaterialTheme.typography.titleMedium)
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "$$total",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
     }
 }
