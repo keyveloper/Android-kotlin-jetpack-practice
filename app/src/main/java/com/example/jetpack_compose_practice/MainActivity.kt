f038bd2a9a2219f144c0a942356ec19a8e8f1acc
zen <didehdrb21@naver.com>
2024-09-14 16:03:48 +0900

    text field component maded & applied

--- app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,17 +13,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetpack_compose_practice.component.InputField
 import com.example.jetpack_compose_practice.ui.theme.DefaultTheme
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { // enter point for emulator // not preview
@@ -43,7 +47,7 @@ class MainActivity : ComponentActivity() {
 fun DefaultPreview() {
     DefaultTheme {
         MyApp {
-            Text("Hello Agai1n")
+            MainContent()
         }
     }
 }
@@ -80,6 +84,45 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
             Text(text = "$$total",
                 style = MaterialTheme.typography.titleMedium,
                 fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    } // check validations
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth()
+        .height(150.dp),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 3.dp, color = Color.LightGray),
+        color = Color(color = 0xFFFFFFFF)
+    ) {
+        Column {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    // Todo - gnvaluechanged
+
+                    keyboardController?.hide()
+                    // close the keyboard
+                }
             )
         }
     }
