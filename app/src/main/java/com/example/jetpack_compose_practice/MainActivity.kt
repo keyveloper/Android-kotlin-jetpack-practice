40574c8b64b84a645fb37f1a87f37b6b866cb593
zen <didehdrb21@naver.com>
2024-09-17 16:35:33 +0900

    start movie app project

--- app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
@@ -4,19 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_practice.ui.theme.JetpackcomposepracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,44 +14,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetpackcomposepracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    JetpackcomposepracticeTheme {
+        content()
+    }
 }
+
+@Composable
+fun MainContent() {
+    Text(text = "Hello")
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MainContent()
+    }
+}
+
 
 @Composable
 fun ShowAge(age: Int = 12) {
     Text(text = age.toString())
 }
 
-@Preview(name = "test name ",showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackcomposepracticeTheme(darkTheme = true) { // there is no layout
-        Surface(
-            modifier = Modifier.fillMaxWidth()
-                .fillMaxHeight()
-                .padding(all = 59.dp),
-            color = MaterialTheme.colorScheme.primary) { // total surface define
-            Column() {
-            Greeting("Android")
-            ShowAge(13)
-                //
-        } }
-    }
-}
