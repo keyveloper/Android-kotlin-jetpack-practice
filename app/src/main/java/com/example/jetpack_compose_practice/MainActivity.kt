6e6041327a2e33c0a14063fc92631b5a7075331d
zen <didehdrb21@naver.com>
2024-09-17 19:27:02 +0900

    scafold - topBar & LazyColumn

--- app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_practice.ui.theme.JetpackcomposepracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,18 +28,52 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     JetpackcomposepracticeTheme {
-        content()
+        // Scaffold to provide the basic structure with a top bar
+        Scaffold(
+            topBar = {
+                MediumTopAppBar(
+                    title = { Text(text = "Movies")},
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                        containerColor = Color.Magenta
+                    )
+                )
+            },
+            content = { innerPadding ->
+                Surface(
+                    modifier = Modifier.padding(innerPadding),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    content()
+                }
+            }
+        )
     }
 }
 
 @Composable
-fun MainContent() {
-    Text(text = "Hello")
-
+fun MainContent(movieList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "Life",
+    "Harry Potter"
+)) {
+    Column(
+       modifier = Modifier.padding(12.dp)
+    ) {
+        LazyColumn {
+            items(
+                items = movieList
+            ) {
+                Text(text = it)
+            }
+        }
+    }
 }
+
 
 
 @Preview(showBackground = true)
