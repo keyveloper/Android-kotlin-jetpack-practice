4b9a7d70cdaad522e8ce117102b4319ca312d78a
zen <didehdrb21@naver.com>
2024-09-26 13:22:22 +0900

    note project start

---
+++ app/src/main/java/com/example/jetpack_compose_practice/screen/NoteScreen.kt
@@ -0,0 +1,15 @@
+package com.example.jetpack_compose_practice.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NoteScreen() {
+
+}
+
+@Preview
+@Composable
+fun NoteScreenPreview() {
+
+}
