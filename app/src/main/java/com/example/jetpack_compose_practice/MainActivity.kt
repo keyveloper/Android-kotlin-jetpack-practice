bd404d6907ba5fcbe595feadee3b432d46f81065
zen <didehdrb21@naver.com>
2024-09-27 22:31:33 +0900

    Trivia Project Start!! for Clean Architecture, Retrofit, UI Design

--- app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
@@ -4,19 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_practice.ui.theme.JetpackcomposepracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +21,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// start
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     JetpackcomposepracticeTheme {
@@ -41,7 +32,6 @@ fun MyApp(content: @Composable () -> Unit) {
 @Composable
 fun MainContent() {
     Text(text = "Hello")
-
 }
 
 
